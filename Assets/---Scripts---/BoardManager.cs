d03fc4c4e765f656a98036c1898a3ea349ae0b88
SamuelC <samuel.charlet62@gmail.com>
2023-10-01 10:26:48 +0200

    Spawn Four Board by number of spawn to keep good number for the bpm

--- Assets/---Scripts---/BoardManager.cs
+++ Assets/---Scripts---/BoardManager.cs
@@ -7,27 +7,41 @@ using UnityEngine.InputSystem.XR;
 
 public class BoardManager : MonoBehaviour
 {
-    [Header("Controls")]
-    [SerializeField] private float _zoomIn = .05f;
+    [Header("Controls")] [SerializeField] private float _zoomIn = .05f;
     [Range(1.1f, 2f)] [SerializeField] private float _zoomOut = 1.2f;
     [SerializeField] private float _dragSpeed;
-    
-    [Header("Board")]
-    [SerializeField] private int _numberToSpawn;
-    [SerializeField] private GameObject _boardEditorPrefab;
-    
-    private List<GameObject> _objectsToMove = new List<GameObject>();
 
+    [Header("Board")] [SerializeField] private int _numberToSpawn;
+    [SerializeField] private GameObject _boardEditorPrefab;
+
+    private List<GameObject> _objectsToMove = new List<GameObject>();
+    private int _counter;
+    private const float StartDistance = 5f;
+    
     private void Start()
     {
         for (int i = 0; i < _numberToSpawn; i++)
         {
+            SpawnFourBoard();
+        }
+    }
+
+    private void SpawnFourBoard()
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            // Instantiate the board
             GameObject go = Instantiate(_boardEditorPrefab, transform);
-            go.GetComponent<BoardEditor>().Init(i+1, i%4);
+            // Init the numbers
+            go.GetComponent<BoardEditor>().Init(_counter+1, j % 4);
+            // Get position of spawner and move it  
             var position = transform.position;
-            go.transform.position = new Vector3(position.x, position.y, position.z + (i * 5f));
+            go.transform.position = new Vector3(position.x, position.y, position.z + (_counter * StartDistance));
+            // Add to list
             _objectsToMove.Add(go);
-        }   
+            // Add to the counter
+            _counter++;
+        }
     }
 
     void Update()
@@ -65,7 +79,7 @@ public class BoardManager : MonoBehaviour
     public void ResetPos()
     {
         transform.position = new Vector3(0, transform.position.y, 0);
-        
+
         for (int i = 0; i < _objectsToMove.Count; i++)
         {
             var position = transform.position;
